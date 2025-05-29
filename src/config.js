6f8cc2ae0fb4544fbe9425abdac07fe4b45be70a
DiegoOrs <diegodaop20@gmail.com>
2025-05-29 12:48:32 -0500

    29-5-25

--- src/config.js
+++ src/config.js
@@ -7,7 +7,5 @@ export const DB_USER = process.env.DB_USER || "ut2ihiym90sg7lch";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "oZJMe7iJwOZy4ED3yrP1";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
-export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "dhttyci5g";
-export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "331911211558835";
-export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "pCcrliHs2YgPtar8-meIIxU0AT4";
+
 
