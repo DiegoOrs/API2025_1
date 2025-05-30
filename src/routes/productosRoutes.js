b5843abacc477476d9543093d43123df8284f520
DiegoOrs <diegodaop20@gmail.com>
2025-05-30 15:46:25 -0500

    Actualizacion 30-5-25

--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -1,8 +1,18 @@
 import { Router } from 'express'
 import { getProductos, getProductosxid, postProductos, putProductos, patchProductos, deleteProductos, upload } from '../controladores/productosCtrl.js';
-
+import multer from 'multer'
 const router = Router();
+//configurar multer para almacenar las imagenes
+    const storage=multer.diskStorage({
+        destination:(req,file,cb)=>{
+            cb(null,'uploads'); //carpeta donde se guardan las imagenes
+        },
+        filename:(req,file,cb)=>{
+            cb(null,`${Date.now()}-${file.originalname}`);
+        }
+    });
 
+    const uploads=multer({storage});
 router.get('/productos', getProductos);
 router.get('/productos/:id', getProductosxid);
 router.post('/productos', upload.single('prod_imagen'), postProductos);
