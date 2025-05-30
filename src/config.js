b09d218c3b04da8c33ad566c76d464c84000ed65
DiegoOrs <diegodaop20@gmail.com>
2025-05-30 17:34:05 -0500

    Actualizacion 30-5-25

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import { config } from 'dotenv'
 config()
 
-export const BD_HOST = process.env.BD_HOST || "b92tvgvyucypcfzbwvx0-mysql.services.clever-cloud.com"; 
-export const BD_DATABASE = process.env.BD_DATABASE || "b92tvgvyucypcfzbwvx0";
-export const DB_USER = process.env.DB_USER || "updi8olpmugcnwlx";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "66uXKU71K6CuPQReqTNh";
+export const BD_HOST = process.env.BD_HOST || "bed0df3ncylkklauzmcs-mysql.services.clever-cloud.com"; 
+export const BD_DATABASE = process.env.BD_DATABASE || "bed0df3ncylkklauzmcs";
+export const DB_USER = process.env.DB_USER || "ufcxx4snikfo9hy4";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "j7nuvVvL7sP7Fhu5z4Cj";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
 
